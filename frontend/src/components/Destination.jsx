67ab78d5b8579128d5d1688008513f4ee7f3ff30
ClemDSC <clemence.p14@gmail.com>
2022-07-21 00:33:23 +0200

    get all destinations in page destination + cardDestination ok

--- frontend/src/components/Destination.jsx
+++ frontend/src/components/Destination.jsx
@@ -1,5 +1,29 @@
 import "../style/Destination.css";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import CardDestination from "./CardDestination";
+
 export default function Destination() {
-  return <div>Hello</div>;
+  const [listDestination, setListDestination] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/destinations`)
+      .then((res) => {
+        setListDestination(res.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul className="listCards">
+        {listDestination.map((destination) => (
+          <li key={destination.id}>
+            <CardDestination key={destination.id} destination={destination} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
