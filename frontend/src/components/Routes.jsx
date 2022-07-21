b1ab314810de3cbefdfeb396a1491200e5c6d115
ClemDSC <clemence.p14@gmail.com>
2022-07-21 21:37:33 +0200

    add wrapper scroll to top au reload des pages

--- frontend/src/components/Routes.jsx
+++ frontend/src/components/Routes.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Wrapper from "../services/Wrapper";
 import Home from "../pages/Home";
 import Destination from "./Destination";
 import Proposition from "./Proposition";
@@ -6,11 +7,13 @@ import Proposition from "./Proposition";
 export default function Main() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/proposition" element={<Proposition />} />
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/proposition" element={<Proposition />} />
+        </Routes>
+      </Wrapper>
     </div>
   );
 }
