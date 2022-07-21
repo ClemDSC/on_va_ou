431d857794d96ba47803a15813c6a9269c287003
ClemDSC <clemence.p14@gmail.com>
2022-07-21 21:12:07 +0200

    function filter ok

--- frontend/src/components/Destination.jsx
+++ frontend/src/components/Destination.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import "../style/Destination.css";
 
 import { useState, useEffect } from "react";
@@ -7,11 +6,7 @@ import axios from "axios";
 import CardDestination from "./CardDestination";
 
 export default function Destination() {
-  const [continent, setContinent] = useState(false);
-  const [pays, setPays] = useState(false);
-  const [ville, setVille] = useState(false);
-  const [language, setlanguage] = useState(false);
-  const [devise, setDevise] = useState(false);
+  const [filterSearch, setFilterSearch] = useState([]);
 
   const [listDestination, setListDestination] = useState([]);
   useEffect(() => {
@@ -24,59 +19,32 @@ export default function Destination() {
 
   return (
     <div className="divlistCards">
-      <div className="divFilter">
-        <label htmlFor="continent">
-          Continent
-          <input
-            className="checkboxFilter"
-            type="checkbox"
-            name="continent"
-            onChange={(e) => setContinent(e.target.value)}
-          />
-        </label>
-        <label htmlFor="continent">
-          Pays
-          <input
-            className="checkboxFilter"
-            type="checkbox"
-            name="continent"
-            onChange={(e) => setPays(e.target.value)}
-          />
-        </label>
-        <label htmlFor="continent">
-          Ville
-          <input
-            className="checkboxFilter"
-            type="checkbox"
-            name="continent"
-            onChange={(e) => setVille(e.target.value)}
-          />
-        </label>
-        <label htmlFor="continent">
-          Langue
-          <input
-            className="checkboxFilter"
-            type="checkbox"
-            name="continent"
-            onChange={(e) => setlanguage(e.target.value)}
-          />
-        </label>
-        <label htmlFor="continent">
-          Devise
-          <input
-            className="checkboxFilter"
-            type="checkbox"
-            name="continent"
-            onChange={(e) => setDevise(e.target.value)}
-          />
-        </label>
+      <div className="divintroFilter">
+        <p className="pintroFilter">
+          Cherche un continent, un pays, une ville, une langue ou une devise :
+        </p>
+        <input
+          className="searching"
+          type="text"
+          placeholder=" 🔎"
+          onChange={(e) => setFilterSearch(e.target.value)}
+        />
       </div>
       <ul className="listCards">
-        {listDestination.map((destination) => (
-          <li key={destination.id} className="liCard">
-            <CardDestination key={destination.id} destination={destination} />
-          </li>
-        ))}
+        {listDestination
+          .filter(
+            (destination) =>
+              destination.continent.includes(filterSearch) ||
+              destination.ville.includes(filterSearch) ||
+              destination.pays.includes(filterSearch) ||
+              destination.devise.includes(filterSearch) ||
+              destination.langue.includes(filterSearch)
+          )
+          .map((destination) => (
+            <li key={destination.id} className="liCard">
+              <CardDestination key={destination.id} destination={destination} />
+            </li>
+          ))}
       </ul>
     </div>
   );
