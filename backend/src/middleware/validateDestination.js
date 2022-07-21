b5753ecc288246e008109365f6e1b83a623890ee
ClemDSC <clemence.p14@gmail.com>
2022-07-21 23:34:11 +0200

    add yup and validateFields middleware in backend

---
+++ backend/src/middleware/validateDestination.js
@@ -0,0 +1,23 @@
+const yup = require("yup");
+
+const validateDestination = async (req, res, next) => {
+  const schema = yup.object().shape({
+    continent: yup.string().max(255).required(),
+    lastnapaysme: yup.string().max(255).required(),
+    photo: yup.string().max(1000).required(),
+    langue: yup.string().max(255).required(),
+    devise: yup.string().max(255).required(),
+    description: yup.string().max(1000).required(),
+    objnom: yup.string().max(255).required(),
+    objcategorie: yup.string().max(255).required(),
+  });
+
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    return next();
+  } catch (err) {
+    return res.status(500).send({ err });
+  }
+};
+
+module.exports = { validateDestination };
